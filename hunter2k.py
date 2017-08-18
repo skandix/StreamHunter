12a8afda0f89e5619cb2e636ba55a1bd84c45f37
Bendik λ <skandix@datapor.no>
2017-08-18 19:08:07 +0200

    Update hunter2k.py

--- hunter2k.py
+++ hunter2k.py
@@ -1,6 +1,5 @@
 import requests
 import shutil
-#import m3u8
 import sys
 import re
 
@@ -9,39 +8,47 @@ urllib3.disable_warnings()
 
 #subject = "is214"
 #baseUrl = "https://live.uia.no/live/ngrp:{0}_all/playlist.m3u8".format(subject)
-baseUrl = "https://httpcache0-47115-httpcache0.dna.qbrick.com/47115-cachelive0/21/0/hls/nrk1/playlist.m3u8"
+#baseUrl = "https://httpcache0-47115-httpcache0.dna.qbrick.com/47115-cachelive0/21/0/hls/nrk1/playlist.m3u8"
+#baseUrl = "https://nrk-live-no.telenorcdn.net/21/0/hls/nrk1/playlist.m3u8"
+baseUrl = "https://nrk-nrk2.akamaized.net/22/0/hls/nrk2/"
 
 def getStream(url, stremState=False):
-	return requests.get(url, stream=stremState)
+	return requests.get(url, stream=stremState, verify=True)
+
+resolution = re.findall(re.compile(ur'(RESOLUTION=\d{1,4}x\d{1,4})'), getStream(baseUrl+"playlist.m3u8").text)
+fps = re.findall(re.compile(ur'(FRAME-RATE=\d{2})'), getStream(baseUrl+"playlist.m3u8").text)
 
 def chunklist(url): #only used to get the chuks 
 	return re.findall(re.compile(ur'chunklist_[^.]+'), getStream(baseUrl).text)
 
-def m3u8_parser(segments=""):
-	playlist = re.findall(re.compile(ur'#EXT-X-STREAM-INF:[A-Z=0-9,\"x\-.a-z \r\n#_]+'), getStream(baseUrl).text)
+def m3u8_parser(resolution,fps,debug=False):
+	segments = ""
+	playlist = re.findall(re.compile(ur'#EXT-X-STREAM-INF:[A-Z=0-9,\"x\-.a-z \r\n#_]+'), getStream(baseUrl+"playlist.m3u8").text)
 	for entry in playlist:
-		if "1280x720" in entry and "50" in entry:	
+		if resolution in entry and fps in entry:	
 			segments = entry.split(',')[4].replace('mp4a.40.2"','').replace('#EXT-X-STREAM-INF','').strip() #going to switch out the replace statements with regex later.. just want to get this shit running
-			segmentsUrls = "/".join(baseUrl.split('/')[:8])+"/"+segments
+			segmentsUrls = "/".join(baseUrl.split('/')[:8])+segments
 			tsUrls = getStream(segmentsUrls).text.replace('\n','\r')
 			tsFiles = re.findall(re.compile(ur'([0-9-]+.ts\?version_hash=\d+\w{2})'), tsUrls)
 			for urls in tsFiles:
 				print urls
+				### then download the shits..
+				## then merge them, and convert to a mp4 file.
+				# AND THEN it migth hopefully play
 
+#			for urls in tsFiles:				
+#				with open(urls[:37], 'wb+') as file:						
+#					shutil.copyfileobj(getStream("/".join(baseUrl.split('/')[:8])+"/"+urls, True).raw, file)
+#				print "{:s}".format(urls)
 
+if getStream(baseUrl+"playlist.m3u8").status_code == 200:
+	try:
+		m3u8_parser(resolution[4], fps[4])
 
-
-print m3u8_parser()
-
-# this is what we want ...
-#EXT-X-STREAM-INF:BANDWIDTH=5160000,RESOLUTION=1280x720,FRAME-RATE=50.000,CODECS="avc1.640020,mp4a.40.2"
-#1709241829-123787468-prog_index.m3u8
-#EXT-X-STREAM-INF:BANDWIDTH=64000,CODECS="mp4a.40.2"
-#1380275607-1380275607-prog_index.m3u8
-
-# test stream
-# https://httpcache0-47115-httpcache0.dna.qbrick.com/47115-cachelive0/21/0/hls/nrk1/playlist.m3u8?b=500-3500&__b__=1428&__a__=off
-"""while streamLive:
-	with open(subject+".mp4", 'wb+') as file:	
-	    shutil.copyfileobj(getStream(baseUrl, True).raw, file)
-	print "{:s}".format(chunklist(baseUrl))"""
+	except TypeError:
+		print "Avaliable Stream Quality"
+		for k,v in enumerate(resolution):
+			print "{0} {1} - {2}".format(k,v, fps[k])
+else:
+	print "something fucked up"
+	sys.exit(1)
