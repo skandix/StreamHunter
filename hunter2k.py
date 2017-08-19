5c15ab2a32b8a0036ea6f818c3aa7ad03d807cff
Bendik λ <skandix@datapor.no>
2017-08-19 12:14:42 +0200

    Update hunter2k.py

--- hunter2k.py
+++ hunter2k.py
@@ -1,16 +1,19 @@
+from datetime import datetime
+
 import requests
 import shutil
 import sys
 import re
+import os
 
 import urllib3
 urllib3.disable_warnings()
 
 #subject = "is214"
-#baseUrl = "https://live.uia.no/live/ngrp:{0}_all/playlist.m3u8".format(subject)
-#baseUrl = "https://httpcache0-47115-httpcache0.dna.qbrick.com/47115-cachelive0/21/0/hls/nrk1/playlist.m3u8"
+#baseUrl = "https://live.uia.no/live/ngrp:{0}_all/".format(subject)
+baseUrl = "https://httpcache0-47115-httpcache0.dna.qbrick.com/47115-cachelive0/21/0/hls/nrk1/"
 #baseUrl = "https://nrk-live-no.telenorcdn.net/21/0/hls/nrk1/playlist.m3u8"
-baseUrl = "https://nrk-nrk2.akamaized.net/22/0/hls/nrk2/"
+#baseUrl = "https://nrk-nrk2.akamaized.net/22/0/hls/nrk2/"
 
 def getStream(url, stremState=False):
 	return requests.get(url, stream=stremState, verify=True)
@@ -18,10 +21,36 @@ def getStream(url, stremState=False):
 resolution = re.findall(re.compile(ur'(RESOLUTION=\d{1,4}x\d{1,4})'), getStream(baseUrl+"playlist.m3u8").text)
 fps = re.findall(re.compile(ur'(FRAME-RATE=\d{2})'), getStream(baseUrl+"playlist.m3u8").text)
 
-def chunklist(url): #only used to get the chuks 
-	return re.findall(re.compile(ur'chunklist_[^.]+'), getStream(baseUrl).text)
+def dotTS_merger(fileName):
+	dotTS = []
+	unSorted = []
+	
+	# only used to testing the lists to see that it's in the rigth order
+	elements = os.listdir(os.getcwd())
+	for elem in elements:
+		if elem.endswith(".ts"):
+			dotTS.append(elem)
+			unSorted.append(elem)
+			sSortedList = sorted(dotTS)
+
+	for k, v in enumerate(unSorted):
+		if v == sSortedList[k]:
+			#return "O.K!"
+			continue
+
+		else:
+			print "Does not look good, make issue at github."
+			break
+
+			#something is not rigth here...
+	with open(fileName+'.ts', 'wb') as filename:
+		for vids in sSortedList:
+			print vids
+			with open(vids, 'rb') as mergeFile:
+				shutil.copyfileobj(mergeFile, filename)
 
 def m3u8_parser(resolution,fps,debug=False):
+
 	segments = ""
 	playlist = re.findall(re.compile(ur'#EXT-X-STREAM-INF:[A-Z=0-9,\"x\-.a-z \r\n#_]+'), getStream(baseUrl+"playlist.m3u8").text)
 	for entry in playlist:
@@ -32,23 +61,23 @@ def m3u8_parser(resolution,fps,debug=False):
 			tsFiles = re.findall(re.compile(ur'([0-9-]+.ts\?version_hash=\d+\w{2})'), tsUrls)
 			for urls in tsFiles:
 				print urls
-				### then download the shits..
-				## then merge them, and convert to a mp4 file.
-				# AND THEN it migth hopefully play
+				with open(urls[:37], 'wb+') as file:						
+					shutil.copyfileobj(getStream("/".join(baseUrl.split('/')[:8])+"/"+urls, True).raw, file)
+				print "{:s}".format(urls)
 
-#			for urls in tsFiles:				
-#				with open(urls[:37], 'wb+') as file:						
-#					shutil.copyfileobj(getStream("/".join(baseUrl.split('/')[:8])+"/"+urls, True).raw, file)
-#				print "{:s}".format(urls)
+try:
+	if getStream(baseUrl+"playlist.m3u8").status_code == 200:
+		
+		try:
+			m3u8_parser()
 
-if getStream(baseUrl+"playlist.m3u8").status_code == 200:
-	try:
-		m3u8_parser(resolution[4], fps[4])
+		except TypeError:
+			print "Avaliable Stream Quality"
+			for k,v in enumerate(resolution):
+				print "{0} {1} - {2}".format(k,v, fps[k])
 
-	except TypeError:
-		print "Avaliable Stream Quality"
-		for k,v in enumerate(resolution):
-			print "{0} {1} - {2}".format(k,v, fps[k])
-else:
-	print "something fucked up"
-	sys.exit(1)
+	else:
+		print "something fucked up"
+		sys.exit(1)
+except KeyboardInterrupt: #so it will start merging what it downloaded so far, and then delete the other files.
+	dotTS_merger()
